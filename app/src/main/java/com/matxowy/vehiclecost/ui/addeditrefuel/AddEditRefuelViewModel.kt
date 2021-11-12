d20ce99e5d072bdd60758391f4577d8fc4bc115f
matxowy <suskamateusz11@gmail.com>
2021-11-12 19:49:15 +0100

    v.1.2.0
    - Added AddEditRefuel and AddEditRepair fragments and viewmodels
    - Added appearance of statistics fragment
    - Added expandable floating action button in statistics fragment
    - Added 3 icons

---
+++ app/src/main/java/com/matxowy/vehiclecost/ui/addeditrefuel/AddEditRefuelViewModel.kt
@@ -0,0 +1,7 @@
+package com.matxowy.vehiclecost.ui.addeditrefuel
+
+import androidx.lifecycle.ViewModel
+
+class AddEditRefuelViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/matxowy/vehiclecost/ui/addeditrepair/AddEditRepairViewModel.kt
@@ -0,0 +1,7 @@
+package com.matxowy.vehiclecost.ui.addeditrepair
+
+import androidx.lifecycle.ViewModel
+
+class AddEditRepairViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
